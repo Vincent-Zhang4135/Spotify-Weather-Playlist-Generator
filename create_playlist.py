9844602508a408dc14317fb8e45171245165ebe6
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-04-02 14:26:58 -0500

    create playlist file

---
+++ create_playlist.py
@@ -0,0 +1,5 @@
+import requests
+
+USER_ID = 'https://api.spotify.com/v1/me'
+SPOTIFY_CREATE_PLAYLIST_URL = 'https://api.spotify.com/v1/users/{USER_ID}/playlists'
+ACCESS_TOKEN = 'BQB8vULEQcwWz6VCYn5Pu7ntFeRkPg12GKwg03-7KmTu7u5yuiKFycI-dZo08JDwXGVfgiiwGB2xnZ52UcJgVRRverQJLk9NxQcRNEgMF4CLJ97DJGCSg-29mMNk80AbMs_ylRPlyUf-L7X-GEd2f_MXDt-XRoDQKsnF6H1-lJj8LdRKrVv5JX0CjF_WZq5XLyk'
